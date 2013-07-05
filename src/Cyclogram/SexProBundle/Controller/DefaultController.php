6c3bd4add64b5c35c31b797760e092e137e273b0
Bohdan Slipenkyi <igaveback@gmail.com>
2013-07-05 14:03:33 +0300

    newsletter template

--- src/Cyclogram/SexProBundle/Controller/DefaultController.php
+++ src/Cyclogram/SexProBundle/Controller/DefaultController.php
@@ -78,12 +78,33 @@ class DefaultController extends Controller
     }
     
     /**
-     * @Route("/newslatter/")
+     * @Route("/newslatter")
      * @Template()
      */
     public function newslatterAction()
     {
-        return $this->render('CyclogramSexProBundle:Default:_newsletter.html.twig');
+        $parameters["email"] = array(
+                'title' => 'E-mail title placeholder', 
+                'info' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat 
+                           volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
+                           Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at 
+                           vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. 
+                           Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non 
+                           habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius 
+                           quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam 
+                           littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. 
+                           Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.',
+                'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique, enim vel pretium commodo, dui purus cursus tellus, vel sodales 
+                           nisl elit a arcu. Curabitur ultrices aliquam nisi, in venenatis arcu porta vitae. Sed hendrerit mollis nibh vel faucibus.'
+                );
+        $parameters["surveys"] = array(
+                array('title' => 'iTest@Home Public Survey',
+                      'content' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt',
+                      'image' => 'newsletter/images/newsletter_tmp_image.jpg'
+                )
+        );
+        
+        return $this->render('CyclogramSexProBundle:Default:_newsletter.html.twig', $parameters);
     }
     
     /**
