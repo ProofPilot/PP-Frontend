ff40007059c853bfb0a15c0342deb2a6a9ac66c0
Bohdan Slipenkyi <igaveback@gmail.com>
2013-07-08 12:46:07 +0300

    study template

--- src/Cyclogram/SexProBundle/Controller/DefaultController.php
+++ src/Cyclogram/SexProBundle/Controller/DefaultController.php
@@ -297,6 +297,31 @@ class DefaultController extends Controller
                            aliquip ex ea commodo consequat.'
         );
         
+        $parameters["itemlist"] = array(
+                array('items' => '1. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'),
+                array('items' => '2. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.'),
+                array('items' => '3. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.')
+        );
+        
+        $parameters["study"] = array(
+                'title' => 'Study Title from study CMS',
+                'subtitle' => 'Study Subtitle from CMS'
+        );
+        
+        $parameters["content"] = array(
+                array('title' => 'Consent information from Study CMS',
+                      'text' => 'This material should come from the CMS and have limited formating forms.  Heading, body, underline, bold, italics, bullets, and numbering. 
+                                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat 
+                                 volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'),
+                array('title' => 'What’s Involved?',
+                      'text' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                                 Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel 
+                                 eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan 
+                                 et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta 
+                                 nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam;')
+                
+        );
+        
         return $this->render('CyclogramSexProBundle:Default:study.html.twig', $parameters);
     }
     
