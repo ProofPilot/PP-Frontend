58b900c0a4c2476b1091dfaf622047da4b426144
Bogdan Dubyk <bogdan.dubyk@gmail.com>
2013-07-10 14:39:10 +0300

    Added knp menus - step 2

--- src/Cyclogram/SexProBundle/Menu/MenuBuilder.php
+++ src/Cyclogram/SexProBundle/Menu/MenuBuilder.php
@@ -29,11 +29,11 @@ class MenuBuilder extends ContainerAware
         $menu = $factory->createItem('root');
         $menu->setChildrenAttribute('class', 'left_menu');
         
-        $menu->addChild('Dashboard', array('route' => '_main'))->setAttribute('class', 'icon_dashboard active');
+        $menu->addChild('Dashboard', array('route' => '_main'))->setAttribute('class', 'icon_dashboard active')->setAttribute("news", 5);
         $menu->addChild('Survey', array('route' => '_survey'))->setAttribute('class', 'icon_survey');
         $menu->addChild('Activities', array('route' => '_study'))->setAttribute('class', 'icon_activities');
         $menu->addChild('Measurements', array('route' => '_secure'))->setAttribute('class', 'icon_measurments');
-        $menu->addChild('Treatment', array('route' => '_newslatter'))->setAttribute('class', 'icon_treatment');
+        $menu->addChild('Treatment', array('route' => '_newsletter'))->setAttribute('class', 'icon_treatment');
 
         return $menu;
     }
