0ef3f4a883bebcdc1de44c60a42d0db18a5e8c65
Bohdan Slipenkyi <igaveback@gmail.com>
2013-07-10 17:20:49 +0300

    survey template

--- src/Cyclogram/SexProBundle/Controller/SurveyController.php
+++ src/Cyclogram/SexProBundle/Controller/SurveyController.php
@@ -14,7 +14,49 @@ class SurveyController extends Controller
      */
     public function surveyAction()
     {
-        return $this->render('CyclogramSexProBundle:Survey:survey.html.twig');
+        
+        $parameters = array();
+        
+        $parameters["lastaccess"] = new \DateTime("2013-07-01 10:05:00");
+         
+        $parameters["user"] = array('avatar' => 'images/tmp_avatar.jpg', 'name' => 'Damien Sonser');
+        
+        $parameters["about"] = array('title' => 'About this survey',
+                'info' => 'This survey helps researchers determine what you are up to now - so
+                that we can compare how and if things have changed in the future.
+                Please answer as honestly as possible.&nbsp; '
+        );
+        
+        $parameters["list"] = array(
+                array('item' => '1. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'),
+                array('item' => '2. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.'),
+                array('item' => '3. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.')
+        );
+        
+        $parameters["page"] = array(
+                'image' => 'images/tmp_banner_small.jpg',
+                'title' => 'Know@home baseline study',
+                'subtitle' => 'Surveys allow you to provide feedback, input information all online.'
+        );
+        
+//         $parameters["survey"] = array(
+//                 array('question' => 'How many sexual partners have you had in the past 12 months?',
+//                         'input' => array('Number')
+//                 ),
+//                 array('question' => 'What is your sexual orientation?',
+//                         'radio' => array('Homosexual/Gay', 'Heterosexual/Straight', 'Bisexual', 'Not sure'),
+//                         'radiogroupname' => 'group1'
+//                 ),
+//                 array('question' => 'With whom have you had sex with in the past 12 months?',
+//                         'radio' => array('Men', 'Women', 'Male to Female Trans-gendered', 'Female to Male Trans-gendered', 'Does not apply'),
+//                         'radiogroupname' => 'group1'
+//                 ),
+//                 array('question' => 'Just another question to fill out the empty space',
+//                         'input' => array('Your height', 'Your weight')
+//                 )
+//         );
+        
+        return $this->render('CyclogramSexProBundle:Survey:survey.html.twig', $parameters);
     }
     
     /**
