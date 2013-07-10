ca7cbf742bad4b70c759515e65a8111f63a8a40d
rivaros <rivantsiv@gmail.com>
2013-07-10 23:41:08 +0300

    Migration for adding facebookid field

---
+++ app/DoctrineMigrations/Version20130710232701.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20130710232701 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+        
+        $this->addSql("ALTER TABLE `proofpilot`.`participant` ADD COLUMN `facebookId` VARCHAR(255) NOT NULL  AFTER `status_id`;");
+
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+        
+        $this->addSql("ALTER TABLE `proofpilot`.`participant` DROP COLUMN `facebookId`;");
+    }
+}
