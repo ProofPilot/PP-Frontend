7bd72938b34548795fb8779a976b107d58f06c5d
Jose Pablo Lizano <josedevel@gmail.com>
2013-07-29 17:11:35 -0600

    Adding coy right notice

--- src/Cyclogram/FrontendBundle/Controller/RegistrationController.php
+++ src/Cyclogram/FrontendBundle/Controller/RegistrationController.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+ * This is part of the ProofPilot package.
+*
+* (c)2012-2013 Cyclogram, Inc, West Hollywood, CA <crew@proofpilot.com>
+* ALL RIGHTS RESERVED
+*
+* This software is provided by the copyright holders to Manila Consulting for use on the
+* Center for Disease Control's Evaluation of Rapid HIV Self-Testing among MSM in High
+* Prevalence Cities until 2016 or the project is completed.
+*
+* Any unauthorized use, modification or resale is not permitted without expressed permission
+* from the copyright holders.
+*
+* KnowatHome branding, URL, study logic, survey instruments, and resulting data are not part
+* of this copyright and remain the property of the prime contractor.
+*
+*/
+
 namespace Cyclogram\FrontendBundle\Controller;
 
 use Symfony\Component\HttpKernel\EventListener\ResponseListener;
@@ -36,7 +54,7 @@ class RegistrationController extends Controller
         }
         $request = $this->getRequest();
         $session = $request->getSession();
-        
+
         $em = $this->getDoctrine()->getManager();
         $study = null;
         $studyLogo = "";
