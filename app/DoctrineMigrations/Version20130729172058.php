f9a18355ab339af11845f2b8ab690a29d4e1f77b
Bogdan Dubyk <bogdan.dubyk@gmail.com>
2013-07-29 17:30:47 +0300

    new migration

---
+++ app/DoctrineMigrations/Version20130729172058.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20130729172058 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+        $this->addSql("INSERT INTO `proofpilot`.`participant_timezone` VALUES (1, 'EST New York, USA -5'),
+                (2, 'CST Chicago, USA -6'), (3, 'MST Denver, USA -7'), (4, 'PST Los Angeles, USA -8'),
+                (5, 'AST Anchorage, USA -9'), (6, 'HAT Honolulu, USA -10'), (7, 'BST Brasilia, Brazil -4'),
+                (8, 'PET Lima, Peru -5'), (9, 'GMT London, UK 0');");
+        
+        $this->addSql("INSERT INTO `proofpilot`.`participant_study_reminder` VALUES (1, 'reminder_study_task'), 
+                (2, 'reminder_orders'), (3, 'reminder_other_studies');");
+        
+        $this->addSql("INSERT INTO `proofpilot`.`participant_contact_time` VALUES (1, 'time_early_am'), (2, 'time_morning'),
+                (3, 'time_afternoon'),(4, 'time_early_evening'), (5, 'time_night'),(6, 'time_late_night');");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
