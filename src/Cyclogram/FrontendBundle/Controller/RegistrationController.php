248e0c797ede7afd59333c5d755aad55a2e288b3
Jose Pablo Lizano <josedevel@gmail.com>
2013-07-29 12:52:56 -0600

    Adding participants to default Arm

--- src/Cyclogram/FrontendBundle/Controller/RegistrationController.php
+++ src/Cyclogram/FrontendBundle/Controller/RegistrationController.php
@@ -91,9 +91,29 @@ class RegistrationController extends Controller
                     $participant->setParticipantRole($role);
                     $status = $em->getRepository('CyclogramProofPilotBundle:Status')->find(1);
                     $participant->setStatus($status);
-        
+
                     $em->persist($participant);
                     $em->flush();
+
+                    //Add participants to Default Arm at the moment.
+                    $armData = $em->getRepository('CyclogramProofPilotBundle:Arm')->find( 3 );
+                    $armData = ( ! is_null( $armData )  ) ? $armData : false;
+
+                    $armStatus = $em->getRepository('CyclogramProofPilotBundle:Status')->find( 1 );
+                    $armStatus = ( ! is_null( $armStatus ) ) ? $armStatus : false;
+
+                    $ArmParticipantLink = null;
+                    if( $armData ){
+                        $ArmParticipantLink = new \Cyclogram\Bundle\ProofPilotBundle\Entity\ParticipantArmLink();
+                        $ArmParticipantLink->setArm($armData);
+                        $ArmParticipantLink->setParticipant($participant);
+                        $ArmParticipantLink->setStatus($armStatus);
+                        $ArmParticipantLink->setParticipantArmLinkDatetime( new \DateTime("now") );
+                    }
+                    $em->persist($ArmParticipantLink);
+
+                    $em->flush();
+                    
                     if (!empty($study)){
                         if ($study->getEmailVerificationRequired()) {
                         return $this->redirect( $this->generateUrl("reg_step_2", array('id' => $participant->getParticipantId(), 'studyId' => $studyId)));
