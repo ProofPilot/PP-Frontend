3b8b5accc5fad2e02e5a74be8388d42df953cf2b
Bogdan Dubyk <bogdan.dubyk@gmail.com>
2013-08-08 17:28:04 +0300

    LimeSurvey service

---
+++ src/Cyclogram/FrontendBundle/Service/LimeSurvey.php
@@ -0,0 +1,15 @@
+<?php
+namespace Cyclogram\FrontendBundle\Service;
+
+use Symfony\Component\DependencyInjection\Container;
+
+class LimeSurvey
+{
+    private $container;
+    
+    public function __construct (Container $container)
+    {
+        $this->container = $container;
+    }
+
+}
