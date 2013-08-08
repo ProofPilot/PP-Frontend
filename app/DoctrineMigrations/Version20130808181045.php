f78103b9798ea8390eb3aa68d2cceb7257a3a7a2
Bogdan Dubyk <bogdan.dubyk@gmail.com>
2013-08-08 19:01:51 +0300

    migration fix

--- app/DoctrineMigrations/Version20130808181045.php
+++ app/DoctrineMigrations/Version20130808181045.php
@@ -14,7 +14,10 @@ class Version20130808181045 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
-        $this->addSql("ALTER TABLE `proofpilot`.`participant` MODIFY COLUMN `country_id`  int(10) UNSIGNED NULL AFTER `participant_zipcode`,
+        $this->addSql("ALTER TABLE `proofpilot`.`participant` ADD COLUMN `education`  varchar(255) NULL AFTER `city`,
+                                                              ADD COLUMN `income`  int(10) NULL AFTER `education`,
+                                                              ADD COLUMN `age`  int(10) NULL AFTER `income`,
+                                                              MODIFY COLUMN `country_id`  int(10) UNSIGNED NULL AFTER `participant_zipcode`,
                                                               MODIFY COLUMN `state_id`  int(10) UNSIGNED NULL AFTER `country_id`,
                                                               MODIFY COLUMN `city_id`  int(10) UNSIGNED NULL AFTER `state_id`,
                                                               MODIFY COLUMN `sex_id`  int(10) UNSIGNED NULL AFTER `city_id`,
@@ -25,10 +28,13 @@ class Version20130808181045 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
-        $this->addSql("ALTER TABLE `proofpilot`.`participant` MODIFY COLUMN `sex_id`  int(10) UNSIGNED NULL AFTER `city_id`,
-                                                              MODIFY COLUMN `race_id`  int(10) UNSIGNED NULL AFTER `sex_id,
-                                                              MODIFY COLUMN `country_id`  int(10) UNSIGNED NULL AFTER `participant_zipcode`,
-                                                              MODIFY COLUMN `state_id`  int(10) UNSIGNED NULL AFTER `country_id`,
-                                                              MODIFY COLUMN `city_id`  int(10) UNSIGNED NULL AFTER `state_id`;");
+        $this->addSql("ALTER TABLE `proofpilot`.`participant` DROP COLUMN `education`,
+                                                              DROP COLUMN `income`,
+                                                              DROP COLUMN `age`,
+                                                              MODIFY COLUMN `country_id`  int(10) UNSIGNED NOT NULL AFTER `participant_zipcode`,
+                                                              MODIFY COLUMN `state_id`  int(10) UNSIGNED NOT NULL AFTER `country_id`,
+                                                              MODIFY COLUMN `city_id`  int(10) UNSIGNED NOT NULL AFTER `state_id`,
+                                                              MODIFY COLUMN `sex_id`  int(10) UNSIGNED NOT NULL AFTER `city_id`,
+                                                              MODIFY COLUMN `race_id`  int(10) UNSIGNED NOT NULL AFTER `sex_id`;");
     }
 }
