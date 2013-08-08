966ade8248c89131fad3f1b986f7d6b70dc0cecc
Bogdan Dubyk <bogdan.dubyk@gmail.com>
2013-08-08 18:25:43 +0300

    migrations

---
+++ app/DoctrineMigrations/Version20130808181045.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20130808181045 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+        $this->addSql("ALTER TABLE `proofpilot`.`participant` MODIFY COLUMN `country_id`  int(10) UNSIGNED NULL AFTER `participant_zipcode`,
+                                                              MODIFY COLUMN `state_id`  int(10) UNSIGNED NULL AFTER `country_id`,
+                                                              MODIFY COLUMN `city_id`  int(10) UNSIGNED NULL AFTER `state_id`,
+                                                              MODIFY COLUMN `sex_id`  int(10) UNSIGNED NULL AFTER `city_id`,
+                                                              MODIFY COLUMN `race_id`  int(10) UNSIGNED NULL AFTER `sex_id`;");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+        $this->addSql("ALTER TABLE `proofpilot`.`participant` MODIFY COLUMN `sex_id`  int(10) UNSIGNED NULL AFTER `city_id`,
+                                                              MODIFY COLUMN `race_id`  int(10) UNSIGNED NULL AFTER `sex_id,
+                                                              MODIFY COLUMN `country_id`  int(10) UNSIGNED NULL AFTER `participant_zipcode`,
+                                                              MODIFY COLUMN `state_id`  int(10) UNSIGNED NULL AFTER `country_id`,
+                                                              MODIFY COLUMN `city_id`  int(10) UNSIGNED NULL AFTER `state_id`;");
+    }
+}
