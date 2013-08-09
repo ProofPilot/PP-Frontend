c4de9bfb554e3207e659d418d74a35fdc0b6dc47
rivaros <rivantsiv@gmail.com>
2013-08-09 12:29:12 +0300

    Added Mike's migrations

---
+++ app/DoctrineMigrations/Version20130806111441.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * This is part of the ProofPilot package.
+*
+* (c)2012-2013 Cyclogram, Inc, West Hollywood, CA <crew@proofpilot.com>
+* ALL RIGHTS RESERVED
+*
+* This software is provided by the copyright holders to Manila Consulting for use on the
+* Center for Disease Control's Evaluation of Rapid HIV Self-Testing among MSM in High
+* Prevalence Cities until 2016 or the project is completed.
+*
+* Any unauthorized use, modification or resale is not permitted without expressed permission
+* from the copyright holders.
+*
+* KnowatHome branding, URL, study logic, survey instruments, and resulting data are not part
+* of this copyright and remain the property of the prime contractor.
+*
+*/
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20130806111441 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    	$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+    	$this->addSql("ALTER TABLE proofpilot.organization CHANGE COLUMN organization_name organization_name VARCHAR(255) NOT NULL");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    	$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+    	$this->addSql("ALTER TABLE proofpilot.organization CHANGE COLUMN organization_name organization_name VARCHAR(255) NOT NULL");
+    	
+    }
+}
---
+++ app/DoctrineMigrations/Version20130808002524.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This is part of the ProofPilot package.
+*
+* (c)2012-2013 Cyclogram, Inc, West Hollywood, CA <crew@proofpilot.com>
+* ALL RIGHTS RESERVED
+*
+* This software is provided by the copyright holders to Manila Consulting for use on the
+* Center for Disease Control's Evaluation of Rapid HIV Self-Testing among MSM in High
+* Prevalence Cities until 2016 or the project is completed.
+*
+* Any unauthorized use, modification or resale is not permitted without expressed permission
+* from the copyright holders.
+*
+* KnowatHome branding, URL, study logic, survey instruments, and resulting data are not part
+* of this copyright and remain the property of the prime contractor.
+*
+*/
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20130808002524 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+    	$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+    	$this->addSql("ALTER TABLE proofpilot.user DROP COLUMN user_type_id");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    	$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+    	$this->addSql("ALTER TABLE proofpilot.user ADD COLUMN user_type_id int(10) unsigned NULL DEFAULT 0");
+    	
+    }
+}
