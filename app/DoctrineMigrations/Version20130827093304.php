06d1dc5033bfb30e02fb1fada75350c6556bdd70
rivaros <rivantsiv@gmail.com>
2013-08-28 16:22:56 +0300

    Ported migrations

---
+++ app/DoctrineMigrations/Version20130827093304.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20130827093304 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+    	$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+    	$this->addSql("ALTER TABLE proofpilot.arm ADD COLUMN arm_default TINYINT(3) UNSIGNED NOT NULL DEFAULT 0  AFTER status_id");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    	$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+    	$this->addSql("ALTER TABLE proofpilot.arm DROP COLUMN arm_default");
+    }
+}
---
+++ app/DoctrineMigrations/Version20130827113525.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20130827113525 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+    	$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+    	$this->addSql("UPDATE arm_intervention_link SET arm_intervention_link_first_study_task = '1' WHERE arm_intervention_link IN (3,10,11)");
+    	
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    	$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+    	$this->addSql("UPDATE arm_intervention_link SET arm_intervention_link_first_study_task = '0' WHERE arm_intervention_link IN (3,10,11)");
+    }
+}
