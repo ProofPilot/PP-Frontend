8975c88a96624ef488aeb4481815ecdf40db0dbe
rivaros <rivantsiv@gmail.com>
2013-09-10 20:26:42 +0300

    fix migration

--- app/DoctrineMigrations/Version20130910184253.php
+++ app/DoctrineMigrations/Version20130910184253.php
@@ -15,30 +15,37 @@ class Version20130910184253 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
         $this->addSql("ALTER TABLE `proofpilot`.`adverse_reaction`
-                DROP FOREIGN KEY `adverse_reaction_ibfk_2`;
-                ALTER TABLE `proofpilot`.`participant_arm_link`
-                ADD CONSTRAINT `adverse_reaction_ibfk_2`
+                DROP FOREIGN KEY `fk_adverse_reaction_participant1`;
+                ALTER TABLE `proofpilot`.`adverse_reaction`
+                ADD CONSTRAINT `fk_adverse_reaction_participant1`
+                FOREIGN KEY (`participant_id` )
+                REFERENCES `proofpilot`.`participant` (`participant_id` )
+                ON DELETE CASCADE;");
+        $this->addSql("ALTER TABLE `proofpilot`.`order_specimen_link`
+                DROP FOREIGN KEY `fk_order_specimen_link_orders1`;
+                ALTER TABLE `proofpilot`.`order_specimen_link`
+                ADD CONSTRAINT `fk_order_specimen_link_orders1`
                 FOREIGN KEY (`participant_id` )
                 REFERENCES `proofpilot`.`participant` (`participant_id` )
                 ON DELETE CASCADE;");
         $this->addSql("ALTER TABLE `proofpilot`.`orders`
-                DROP FOREIGN KEY `orders_ibfk_4`;
+                DROP FOREIGN KEY `fk_order_participant1`;
                 ALTER TABLE `proofpilot`.`orders`
-                ADD CONSTRAINT `orders_ibfk_4`
+                ADD CONSTRAINT `fk_order_participant1`
                 FOREIGN KEY (`participant_id` )
                 REFERENCES `proofpilot`.`participant` (`participant_id` )
                 ON DELETE CASCADE;");
         $this->addSql("ALTER TABLE `proofpilot`.`appointment`
-                DROP FOREIGN KEY `appointment_ibfk_5`;
+                DROP FOREIGN KEY `fk_appointment_participant1`;
                 ALTER TABLE `proofpilot`.`appointment`
-                ADD CONSTRAINT `appointment_ibfk_5`
+                ADD CONSTRAINT `fk_appointment_participant1`
                 FOREIGN KEY (`participant_id` )
                 REFERENCES `proofpilot`.`participant` (`participant_id` )
                 ON DELETE CASCADE;");
         $this->addSql("ALTER TABLE `proofpilot`.`sex_with`
-                DROP FOREIGN KEY `sex_with_ibfk_1`;
+                DROP FOREIGN KEY `fk_sex_with_participant1`;
                 ALTER TABLE `proofpilot`.`sex_with`
-                ADD CONSTRAINT `sex_with_ibfk_1`
+                ADD CONSTRAINT `fk_sex_with_participant1`
                 FOREIGN KEY (`participant_id` )
                 REFERENCES `proofpilot`.`participant` (`participant_id` )
                 ON DELETE CASCADE;");
