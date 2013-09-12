cb22a17d60515214e77fa48b7a3f97d636aecc8f
Bogdan Dubyk <bogdan.dubyk@gmail.com>
2013-09-12 12:59:21 +0300

    migration fix

--- app/DoctrineMigrations/Version20130912111023.php
+++ app/DoctrineMigrations/Version20130912111023.php
@@ -14,8 +14,9 @@ class Version20130912111023 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
-        $this->addSql("ALTER TABLE `proofpilot`.`participant` ADD COLUMN `voice_phone` INT(10) NULL  AFTER `language`;
-                UPDATE `proofpilot`.`participant` SET `language` = 'en' WHERE `language`is NULL;");
+        $this->addSql("ALTER TABLE `proofpilot`.`participant` ADD COLUMN `voice_phone` INT(10) NULL  AFTER `language`;");
+        $this->addSql("UPDATE `proofpilot`.`participant` SET `language` = 'en' WHERE `language`is NULL;");
+        $this->addSql("ALTER TABLE `proofpilot`.`participant` MODIFY COLUMN `language`  VARCHAR(255) NOT NULL DEFAULT 'en';");
     }
 
     public function down(Schema $schema)
@@ -23,5 +24,6 @@ class Version20130912111023 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
         $this->addSql("ALTER TABLE `proofpilot`.`participant` DROP COLUMN `voice_phone`;");
+        $this->addSql("ALTER TABLE `proofpilot`.`participant` MODIFY COLUMN `language` VARCHAR(255) NULL");
     }
 }
