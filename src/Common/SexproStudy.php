b5ce1dda2d96868ef84a4c3b31a6065a7799ca1c
ddobei <dmytro.dobei@gmail.com>
2013-09-24 15:22:50 +0300

    added copyright to all frontend php files

--- src/Common/SexproStudy.php
+++ src/Common/SexproStudy.php
@@ -1,4 +1,21 @@
 <?php
+/*
+* This is part of the ProofPilot package.
+*
+* (c)2012-2013 Cyclogram, Inc, West Hollywood, CA <crew@proofpilot.com>
+* ALL RIGHTS RESERVED
+*
+* This software is provided by the copyright holders to Manila Consulting for use on the
+* Center for Disease Control's Evaluation of Rapid HIV Self-Testing among MSM in High
+* Prevalence Cities until 2016 or the project is completed.
+*
+* Any unauthorized use, modification or resale is not permitted without expressed permission
+* from the copyright holders.
+*
+* KnowatHome branding, URL, study logic, survey instruments, and resulting data are not part
+* of this copyright and remain the property of the prime contractor.
+*
+*/
 namespace Common;
 use Cyclogram\Bundle\ProofPilotBundle\Entity\Study;
 
--- src/Cyclogram/FrontendBundle/Controller/RegistrationController.php
+++ src/Cyclogram/FrontendBundle/Controller/RegistrationController.php
@@ -1,11 +1,11 @@
 <?php
-
 /*
- * This is part of the ProofPilot package.
+* This is part of the ProofPilot package.
 *
 * (c)2012-2013 Cyclogram, Inc, West Hollywood, CA <crew@proofpilot.com>
 * ALL RIGHTS RESERVED
 *
+* This software is provided by the copyright holders to Manila Consulting for use on the
 * Center for Disease Control's Evaluation of Rapid HIV Self-Testing among MSM in High
 * Prevalence Cities until 2016 or the project is completed.
 *
