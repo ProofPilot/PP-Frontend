951ca2e7aeb3b8676355f0824ba8f0761c619226
Bogdan Dubyk <bogdan.dubyk@gmail.com>
2013-10-17 18:50:09 +0300

    defaultParticipantStudyLogic fix

--- src/Common/DefaultParticipantStudy.php
+++ src/Common/DefaultParticipantStudy.php
@@ -67,24 +67,36 @@ class DefaultParticipantStudy extends AbstractStudy
         $participantInterventionLink->setStatus(ParticipantInterventionLink::STATUS_ACTIVE);
         $em->persist($participantInterventionLink);
         $em->flush();
-        //DefaultParticipantCommunicationPreferencesInterventionLink
-        $participantInterventionLink = new \Cyclogram\Bundle\ProofPilotBundle\Entity\ParticipantInterventionLink();
-        $intervention = $em
-            ->getRepository('CyclogramProofPilotBundle:Intervention')
-            ->findOneByInterventionCode('DefaultParticipantShippingInformation');
-        $participantInterventionLink->setIntervention($intervention);
-        $participantInterventionLink->setParticipant($participant);
-        $participantInterventionLink
-            ->setParticipantInterventionLinkDatetimeStart(new \DateTime("now"));
-        $participantInterventionLink->setStatus(ParticipantInterventionLink::STATUS_ACTIVE);
-        $em->persist($participantInterventionLink);
-        $em->flush();
     }
     public function participantDefaultInterventionLogic($participant, $update = null)
     {
         $em = $this->container->get('doctrine')->getManager();
+        $shippingInformation = false;
+        $shippingInformationintervention = $em
+        ->getRepository('CyclogramProofPilotBundle:Intervention')->findOneByInterventionCode('DefaultParticipantShippingInformation');
+        $existshippingInformation = $studies = $em->getRepository('CyclogramProofPilotBundle:ParticipantInterventionLink')
+            ->checkIfExistParticipantInterventionLink($shippingInformationintervention-> getInterventionCode(), $participant->getParticipantId());
+        $studies = $em->getRepository('CyclogramProofPilotBundle:Participant')
+        ->getEnrolledStudies($participant);
+        foreach ($studies as $st) {
+            if ($st->getEmailVerificationRequired())
+                $shippingInformation = true;
+        }
+        if ($shippingInformation == true && $existshippingInformation == false) {
+            $participantInterventionLink = new \Cyclogram\Bundle\ProofPilotBundle\Entity\ParticipantInterventionLink();
+
+            $participantInterventionLink->setIntervention($shippingInformationintervention);
+            $participantInterventionLink->setParticipant($participant);
+            $participantInterventionLink
+                ->setParticipantInterventionLinkDatetimeStart(new \DateTime("now"));
+            $participantInterventionLink->setStatus(ParticipantInterventionLink::STATUS_ACTIVE);
+            $em->persist($participantInterventionLink);
+            $em->flush();
+        }
+        
         $study = $em->getRepository('CyclogramProofPilotBundle:Study')
            ->findOneByStudyCode($this->getStudyCode());
+        
         //get all participant intervention links
         $interventionLinks = $em
                 ->getRepository('CyclogramProofPilotBundle:ParticipantInterventionLink')
