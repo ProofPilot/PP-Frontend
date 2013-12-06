7be4200528cef5762ff3d65865a02ccbd4b83ed6
Bogdan Dubyk <bogdan.dubyk@gmail.com>
2013-12-06 15:13:36 +0200

    migration fix

--- app/DoctrineMigrations/Version20131204153204.php
+++ app/DoctrineMigrations/Version20131204153204.php
@@ -13,7 +13,7 @@ class Version20131204153204 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
-        $this->addSql("CREATE  TABLE IF NOT EXISTS `new-roofpilot`.`study_promo_code` (
+        $this->addSql("CREATE  TABLE IF NOT EXISTS `new-proofpilot`.`study_promo_code` (
                           `study_promo_code_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
                           `study_id` INT(50) UNSIGNED NOT NULL ,
                           `study_promo_code_value` VARCHAR(45) NOT NULL ,
@@ -22,7 +22,7 @@ class Version20131204153204 extends AbstractMigration
                 		  INDEX `fk_study_promo_code_study1_idx` (`study_id`) ,
                           CONSTRAINT `fk_study_promo_code_study1`
 		                    FOREIGN KEY (`study_id`)
-		                    REFERENCES `proofpilot`.`study` (`study_id`)
+		                    REFERENCES `new-proofpilot`.`study` (`study_id`)
 		                    ON DELETE NO ACTION
 		                    ON UPDATE NO ACTION)
                         ENGINE = InnoDB");
