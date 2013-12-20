8c494575e3f600c3affb9e0723560b6d86431e22
Bogdan Dubyk <bogdan.dubyk@gmail.com>
2013-12-20 15:59:53 +0200

    admin migrations

---
+++ app/DoctrineMigrations/Version20131217151255.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20131217151255 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+    	$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+    	$this->addSql("ALTER TABLE study_content ADD COLUMN study_sponsor_by VARCHAR(2000) NULL DEFAULT NULL  AFTER study_proofpilot_privacy");
+    	
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    	$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+    	$this->addSql("ALTER TABLE study_content DROP COLUMN study_sponsor_by");
+
+    }
+}
