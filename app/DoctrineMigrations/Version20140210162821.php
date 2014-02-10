8917f67f55cae131422733bdae50a53262af84d1
Dmytro Dobei <dmytro.dobei@gmail.com>
2014-02-10 16:30:28 +0200

    default location_organization_link

---
+++ app/DoctrineMigrations/Version20140210162821.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20140210162821 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+    	$this->addSql("
+						INSERT INTO `new-proofpilot`.`location_organization_link`
+						(`location_id`,
+						`organization_id`)
+						VALUES
+						((SELECT location_id FROM `new-proofpilot`.location where location_name LIKE 'Sunset Plaza Dental%'),
+						(SELECT organization_id FROM `new-proofpilot`.organization where organization_name LIKE 'ProofPilot%')),
+						((SELECT location_id FROM `new-proofpilot`.location where location_name LIKE 'Drug Rehabilitation Center in West Hollywood%'),
+						(SELECT organization_id FROM `new-proofpilot`.organization where organization_name LIKE 'Pace University%')),
+						((SELECT location_id FROM `new-proofpilot`.location where location_name LIKE 'Stone Skin Care%'),
+						(SELECT organization_id FROM `new-proofpilot`.organization where organization_name LIKE 'Georgia State University%')),
+						((SELECT location_id FROM `new-proofpilot`.location where location_name LIKE 'Jessica Nail Clinic%'),
+						(SELECT organization_id FROM `new-proofpilot`.organization where organization_name LIKE 'University of Kentucky%')),
+						((SELECT location_id FROM `new-proofpilot`.location where location_name LIKE 'Back to Total Health%'),
+						(SELECT organization_id FROM `new-proofpilot`.organization where organization_name LIKE 'A Research Institution%')),
+						((SELECT location_id FROM `new-proofpilot`.location where location_name LIKE 'Sunset Acupuncture - META CLINIC%'),
+						(SELECT organization_id FROM `new-proofpilot`.organization where organization_name LIKE 'Abt Associates%')),
+						((SELECT location_id FROM `new-proofpilot`.location where location_name LIKE 'Saban Community Clinic - Beverly Health Cente%'),
+						(SELECT organization_id FROM `new-proofpilot`.organization where organization_name LIKE 'Test%')),
+						((SELECT location_id FROM `new-proofpilot`.location where location_name LIKE 'James Guay%'),
+						(SELECT organization_id FROM `new-proofpilot`.organization where organization_name LIKE 'New York Blood Center%'));
+    			");
+
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
